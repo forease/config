a019b3ba557a6bee4e9e5e1631d1859311ba2e73
实易科技 <yangmingmao@forease.net>
2014-11-27 23:26:41 +0800

    Create config_test.go

---
+++ config_test.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	//"errors"
+	//"flag"
+	//"strings"
+	"fmt"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	c, _ := NewConfig("toml.conf", 5)
+	fmt.Printf("%v\n\n======\n\n", c.Item)
+	fmt.Println("String:")
+	fmt.Println(c.String("servers.alpha.ip", "default"))
+	fmt.Println("Int64:")
+	fmt.Println(c.Int64("database.connection_max", 0))
+	fmt.Println("Int:")
+	fmt.Println(c.Int("database.connection_max2", 10))
+	fmt.Println("Bool:")
+	fmt.Println(c.Bool("database.enabled", false))
+	fmt.Println(c.Bool("database.enabled2", false))
+	fmt.Println("Float64:")
+	fmt.Println(c.Float64("owner.float", 0.12))
+
+	fmt.Println(c.Array("clients.hosts"))
+}
